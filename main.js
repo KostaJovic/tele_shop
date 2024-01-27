2e5fb0f64c1382a1e3e349e44041281db0ba8076
David <David.Hermeter1806@berufsschule.bz>
2024-01-27 16:05:50 +0100

    solved

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const { Telegraf } = require('telegraf')
 const { message } = require('telegraf/filters')
 
 //token = "6724277597:AAGVODrxB0knWmJJtCKOmnGGxE7iKl_glKU"
-//const bot = new Telegraf(process.env.BOT_TOKEN)
+//const token = "6724277597:AAGVODrxB0knWmJJtCKOmnGGxE7iKl_glKU";
+//const bot = new Telegraf(token);
 
 const token = process.env.BOT_TOKEN;
 const bot = new Telegraf(token);
@@ -14,14 +15,21 @@ const http = require('http');
 http.createServer(function (req, res) {
     initialiseBot();
 }).listen(8080);
- 
+
 function initialiseBot() {
+    // Register middleware and launch your bot as usual
+    bot.use((ctx, next) => {
+        // Yay, `myProp` is now available here as `string | undefined`!
+        ctx.myProp = ctx.chat?.first_name?.toUpperCase()
+        return next()
+    })
+
     bot.start((ctx) => ctx.reply('Hi, ready to buy some products?'));
     bot.help((ctx) => ctx.reply('Send me a sticker'));
     //bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
 
     bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-    bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+    bot.hears('sticker', (ctx) => ctx.reply('sticker'));
     bot.launch();
 
     // Enable graceful stop
